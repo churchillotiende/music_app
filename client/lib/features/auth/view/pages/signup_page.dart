9db14df2602e243a34f1b870a139eb4a3c8cda6b
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-25 08:57:53 +0300

    setup: initial commit

---
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          const Text(
+            "Sign Up.",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
