62218a73a5cae2991caa284299765839a8393569
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-25 10:20:29 +0300

    chore: moves the duplicate logic to a reusable property _boarder

--- client/lib/core/theme/theme.dart
+++ client/lib/core/theme/theme.dart
@@ -2,16 +2,15 @@ import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static OutlineInputBorder _boader(Color color) => OutlineInputBorder(
+      borderSide: BorderSide(color: color, width: 3),
+      borderRadius: BorderRadius.circular(10));
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Pallete.backgroundColor,
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: EdgeInsets.all(27),
-      enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Pallete.borderColor, width: 3),
-          borderRadius: BorderRadius.circular(10)),
-      focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Pallete.gradient2, width: 3),
-          borderRadius: BorderRadius.circular(10)),
+      enabledBorder: _boader(Pallete.borderColor),
+      focusedBorder: _boader(Pallete.gradient2),
     ),
   );
 }
