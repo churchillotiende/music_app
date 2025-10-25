31c698615c7c63941ab06f34cb5ed760fbd80571
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-25 09:48:28 +0300

    chore: using the mainAxisAlignment widget

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -13,30 +13,34 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          const Text(
-            "Sign Up.",
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Sign Up.",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          CustomField(
-            hintText: "Name",
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          CustomField(hintText: "Email"),
-          const SizedBox(
-            height: 15,
-          ),
-          CustomField(hintText: "Password"),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            CustomField(
+              hintText: "Name",
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomField(hintText: "Email"),
+            const SizedBox(
+              height: 15,
+            ),
+            CustomField(hintText: "Password"),
+          ],
+        ),
       ),
     );
   }
