3d47025d1a848f3fc51a8f33064c2873a0a197ff
Churchill Onyango Otiende <churchill.otiende@naivas.co.ke>
2025-10-25 11:53:19 +0300

    chore: adds link to login page

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
 import 'package:flutter/material.dart';
@@ -44,6 +45,22 @@ class _SignUpPageState extends State<SignUpPage> {
               height: 15,
             ),
             AuthGradientButton(),
+            const SizedBox(
+              height: 20,
+            ),
+            RichText(
+              text: TextSpan(
+                  text: "Already have an Account ? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                        text: "Sign In",
+                        style: TextStyle(
+                          color: Pallete.gradient2,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ]),
+            ),
           ],
         ),
       ),
